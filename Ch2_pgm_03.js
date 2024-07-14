4c4e2432197fc7108834b9a978072a1f1ebe7dfb
poojalakshmi-1210 <poojalakshmi057@gmail.com>
2024-07-14 13:07:05 +0530

    first commit

---
+++ Ch2_pgm_03.js
@@ -0,0 +1,29 @@
+// Using a variable
+
+var score;
+score = 100;
+console.log(score);
+
+var score;
+score = 150;
+console.log(score);
+
+var score2;
+score2 = 75;
+console.log(score2);
+
+
+
+
+/* Further Adventures
+ *
+ * 1) Change the value assigned to score.
+ *    Run the program again.
+ *
+ * 2) Declare a second variable, maybe score2.
+ *
+ * 3) Assign your variable a value.
+ *
+ * 4) Use console.log to display your variable on the console.
+ *
+ */
---
+++ Ch2_pgm_07.js
@@ -0,0 +1,27 @@
+// Declaring and assigning in one step
+
+var playerName = "Kandra";
+var locationName = "The Dungeon of Doom";
+
+console.log(playerName + " is in " + locationName);
+
+var health = 50;
+var message = playerName + " has health " + health + " and is in " + locationName;
+console.log(message);
+
+
+/* Further Adventures
+ *
+ * 1) Declare a health variable and assign it
+ *    a value in one step.
+ *
+ * 2) Declare a message variable.
+ *
+ * 3) Assign message a value created by joining
+ *    variables and text to say something like
+ *    "Kandra has health 50 and is in The Dungeon of Doom"
+ *
+ * 4) Replace the current console.log statement
+ *    with a new one to display the message variable.
+ *
+ */
